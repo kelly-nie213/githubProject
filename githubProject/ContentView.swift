3689e095a010ae613d92ab6abecf3feaaca8253d
Kelly Nie <kellywnie@gmail.com>
2024-07-26 13:38:59 -0400

    Initial Commit

---
+++ githubProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  githubProject
+//
+//  Created by Kelly Nie on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubProject/githubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubProjectApp.swift
+//  githubProject
+//
+//  Created by Kelly Nie on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct githubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
